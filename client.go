d4d55d678778d8a2b5c724e8d95e93db4a81224f
Brendan O'Brien <sparkle_pony_2000@qri.io>
2017-04-13 15:14:22 -0400

    experimenting with max WebSocket message size

--- client.go
+++ client.go
@@ -22,7 +22,7 @@ const (
 	// Send pings to peer with this period. Must be less than pongWait.
 	pingPeriod = (pongWait * 9) / 10
 	// Maximum message size allowed from peer.
-	maxMessageSize = 512
+	maxMessageSize = 2048
 )
 
 var (
