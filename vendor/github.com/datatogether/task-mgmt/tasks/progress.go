4f971cd4b18a5381e3583cb34f85c1f8ba63e0b2
Brendan O'Brien <sparkle_pony_2000@qri.io>
2017-07-17 14:50:15 -0400

    update godeps

--- vendor/github.com/datatogether/task-mgmt/tasks/progress.go
+++ vendor/github.com/datatogether/task-mgmt/tasks/progress.go
@@ -1,21 +1,12 @@
 package tasks
 
-import (
-	"fmt"
-)
-
 // Progress represents the current state of a task
 // tasks will be given a Progress channel to send updates
 type Progress struct {
-	TaskId  string  `json:"taskId"`
-	Percent float32 `json:"percent"` // percent complete between 0.0 & 1.0
-	Step    int     `json:"step"`    // current Step
-	Steps   int     `json:"steps"`   // number of Steps in the task
-	Status  string  `json:"status"`  // status string that describes what is currently happening
-	Done    bool    `json:"done"`    // complete flag
-	Error   error   `json:"error"`   // error message
-}
-
-func (p Progress) ChannelName() string {
-	return fmt.Sprintf("tasks.%s.progress", p.TaskId)
+	Percent float32 `json:"percent"`         // percent complete between 0.0 & 1.0
+	Step    int     `json:"step"`            // current Step
+	Steps   int     `json:"steps"`           // number of Steps in the task
+	Status  string  `json:"status"`          // status string that describes what is currently happening
+	Done    bool    `json:"done"`            // complete flag
+	Error   error   `json:"error,omitempty"` // error message
 }
